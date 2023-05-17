a7fd8d60bdefb9caf55c52aa640db6fef1ed0322
Shubham Dubey <shubhamshd99@gmail.com>
2023-05-17 16:16:10 +0530

    working on tatkal ticket automation

---
+++ automation.py
@@ -0,0 +1,132 @@
+from __future__ import print_function
+import time
+from tkinter import Frame, Tk, Label, Entry, E, Button, LEFT
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.safari.webdriver import SafariDriver
+
+class BookingGui(Frame, object):
+    def __init__(self, master, labels):
+        """
+        For Creating interactive GUI for IRCTC automation
+        :param master:
+        :param labels:
+        """
+        super(BookingGui, self).__init__(master)
+        self.label = labels
+        self.passanger = ["PassengersDetail:", "Psg:One", "Psg:Two", "Psg:Three", "Psg:Four"]
+        self.entry = {}
+        self.values = {}
+
+    def main_gui(self):
+        """
+        For creating interactive GUI for IRCTC Automation
+        """
+        for label_index, label_value in zip(range(1, 10), self.label):
+            Label(self, text=label_value).grid(row=label_index, column=0)
+            self.entry[label_value] = Entry(self, show="*") if label_value is "Password" else Entry(self)
+            self.entry[label_value].grid(row=label_index, column=1)
+        for passenger_index, passenger_label in zip(range(0, 5), self.passanger):
+            Label(self, text=passenger_label).grid(row=label_index + 1, column=passenger_index)
+        for index, value in zip(range(label_index + 2, 14), self.label[label_index + 1:]):
+            Label(self, text=value).grid(row=index, column=0)
+            for extra_index in range(0, 4):
+                self.entry[value + str(extra_index)] = Entry(self)
+                self.entry[value + str(extra_index)].grid(row=index, column=extra_index + 1)
+        Button(self, text="Book Tatkal Ticket", command=self._login_btn_clicked).grid(row=5, column=3)
+
+    def _login_btn_clicked(self):
+        """
+        It will start the portal with provided value
+        """
+        for label, index in zip(self.label, range(1, 10)):
+            self.values[label] = self.entry[label].get()
+        for temp_index in zip(self.label[10:]):
+            for extra_index in range(0, 4):
+                self.values[temp_index[0] + str(extra_index)] = self.entry[temp_index[0] + str(extra_index)].get()
+        
+        VALUES = {
+            "UserID": "shubhamshd",
+            "Password": "6983@C1rc",
+            "TrainNo": "22178",
+            "FromStation": "MIRZAPUR",
+            "ToStation": "DADAR",
+            "Date": "2023-05-18",
+            "Class": "SL",
+            "Quota": "TATKAL",
+            "MobileNo": "7021282914",
+            "Name0": "Shubham Dubey",
+            "Age0": "24",
+            "Gender0": "Male",
+        }
+        booking = Booking(VALUES)
+        booking.main()
+
+
+class Booking(object):
+    def __init__(self, values):
+        """
+        It will hold the main class for executing crome Browser
+        :param values:
+        """
+        self.browser = None
+        self.values = values
+
+    def main(self):
+        """
+        It will be having main script for crome automation
+        """
+        try:
+            self.browser = webdriver.Chrome()
+            # self.browser = webdriver.Safari()
+            self.browser.get("https://www.irctc.co.in/")
+            # login_page = self.browser.find_element_by_id("loginFormId")
+            # login_page = self.browser.find_element_by_xpath("//a[contains(@class, 'loginText') and contains(text(), 'LOGIN')]")
+            login_page = self.browser.find_element(By.XPATH, "//a[contains(@class, 'loginText') and contains(text(), 'LOGIN')]")
+            # login_page.find_element_by_id("8820571").send_keys(self.values["UserID"])
+            # login_page.find_element(By.NAME, "8820571").send_keys(self.values["UserID"])
+            login_page.find_element(By.CSS_SELECTOR, "input[formcontrolname='userid']").send_keys(self.values["UserID"])
+            
+            # login_page.find_element_by_name("8918335").send_keys(self.values["Password"])
+            # login_page.find_element(By.ID, "8918335").send_keys(self.values["Password"])
+            login_page.find_element(By.CSS_SELECTOR, "input[formcontrolname='password']").send_keys(self.values["Password"])
+            time.sleep(10)
+            login_page.find_element_by_id("loginbutton").click()
+            main_page = self.browser.find_element_by_class_name("container")
+            main_page.find_element_by_id("quickbookTab:header:inactive").click()
+            main_page.find_element_by_id("qbform:trainNUmber").send_keys(self.values["TrainNo"])
+            main_page.find_element_by_id("qbform:fromStation").send_keys(self.values["FromStation"])
+            main_page.find_element_by_id("qbform:toStation").send_keys(self.values["ToStation"])
+            main_page.find_element_by_id("qbform:qbJrnyDateInputDate").send_keys(self.values["Date"])
+            main_page.find_element_by_id("qbform:class").send_keys(self.values["Class"])
+            main_page.find_element_by_id("qbform:quota").send_keys(self.values["Quota"])
+            main_page.find_element_by_id("qbform:quickBookSubmit").click()
+            for index in range(0, 3):
+                self.browser.find_element_by_id(
+                    "addPassengerForm:psdetail:" + str(index) + ":j_idt567").find_element_by_tag_name(
+                    "input").send_keys(
+                    self.values["Name" + str(index)])
+                self.browser.find_element_by_id("addPassengerForm:psdetail:" + str(index) + ":psgnAge").send_keys(
+                    self.values["Age" + str(index)])
+                self.browser.find_element_by_id("addPassengerForm:psdetail:" + str(index) + ":psgnGender").send_keys(
+                    self.values["Gender" + str(index)])
+                self.browser.find_element_by_id("addPassengerForm:mobileNo").send_keys(self.values["MobileNo"])
+            self.browser.find_element_by_id("nlpAnswer").send_keys("")
+            time.sleep(10)
+            self.browser.find_element_by_id("COD").click()
+            self.browser.find_elements_by_css_selector("input[type='radio'][value='100']")[0].click()
+            self.browser.find_element_by_id("validate").click()
+            exit(0)
+        except BaseException as error:
+            raise error
+
+
+if __name__ == '__main__':
+    FIELDS = ["UserID", "Password", "TrainNo", "FromStation", "ToStation", "Date", "Class", "Quota",
+              "MobileNo", "PassengersDetail:", "Name", "Age", "Gender"]
+    ROOT = Tk()
+    BOOKING = BookingGui(ROOT, FIELDS)
+    BOOKING.main_gui()
+    BOOKING.pack(side=LEFT)
+    ROOT.mainloop()
